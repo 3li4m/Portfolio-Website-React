2413dccc640627737517de5efd9c6346fd86a4b5
Elie <elie.ajaka87@gmail.com>
2022-03-30 23:47:12 +1100

    trying changes

--- src/PortfolioContainer/Skills/Skills.js
+++ src/PortfolioContainer/Skills/Skills.js
@@ -4,21 +4,7 @@ import Card from "../../Card.js"
 
 const Skills = () => {
     return( 
-        <section className="projects top" id="projects">
-            <div className="container">
-                <div className="heading">
-                    <h4></h4>
-                    <h1>Skills</h1>
-                </div>
-
-                <div className="content grid">
-                        {data.map((val, index) => {
-                            return <Card key={index.id} image={val.image} title={val.title} desc={val.desc}/>
-                            }
-                        )}
-                </div>
-            </div>
-        </section>
+        <div></div>
     );
 }
 

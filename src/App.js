dd3a64865cbc033a2c920fdd3eaff0ecbe27f988
Elie <elie.ajaka87@gmail.com>
2022-03-31 00:18:19 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Profile from './PortfolioContainer/Home/Profile.js'
 import Header from './PortfolioContainer/Header/Header.js'
 import Projects from './PortfolioContainer/Projects/Projects';
-import Skills from './PortfolioContainer/Skills/Skills';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
           <Header />
           <Profile />
           <Projects />
-          <Skills />
         </div>
     </div>
   );
